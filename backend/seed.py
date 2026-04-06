5a24a144c247ecf442f8e1c7e408bcd580200819
NILASMAC <macadamnilas@gmail.com>
2026-04-06 13:58:38 +0200

    fix seed

--- backend/seed.py
+++ backend/seed.py
@@ -3,12 +3,12 @@ from app.models import User
 
 app = create_app()
 with app.app_context():
-    # On vérifie si l'admin existe déjà pour éviter une erreur
+    db.create_all()
     if not User.query.filter_by(email="admin@stock.com").first():
         a = User(nom="Admin", email="admin@stock.com", role="admin", is_active=True)
         a.set_password("admin123")
         db.session.add(a)
         db.session.commit()
-        print("Félicitations : Utilisateur Admin créé avec succès !")
+        print("Admin créé avec succès !")
     else:
-        print("L'utilisateur admin existe déjà dans la base.")
+        print("Admin existe déjà.")
