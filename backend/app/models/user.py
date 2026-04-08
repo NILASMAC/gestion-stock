292b9eaed1742333cca48d3fee7b0d80e6db0442
laurincekinsly <kingslydebruyne17@gmail.com>
2026-04-08 10:26:40 +0100

    fixing id issue

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -5,7 +5,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 class User(db.Model):
     __tablename__ = 'users'
 
-    id             = db.Column(db.Integer, primary_key=True)
+    # Make autoincrement explicit for PostgreSQL
+    id             = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nom            = db.Column(db.String(100), nullable=False)
     email          = db.Column(db.String(120), unique=True, nullable=False)
     password_hash  = db.Column(db.String(256), nullable=False)
